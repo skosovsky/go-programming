ca8085f6de6b965719be968bd6673adb701bc5c8
Sergey Kosovsky <skosovsky@gmail.com>
2024-03-01 23:48:02 +0300

    Add solution fo delete-num

--- delete-num/main.go
+++ delete-num/main.go
@@ -11,6 +11,8 @@ func main() {
 		return
 	}
 
+	deleteNumSmart(num, deletedNum) // Smart
+
 	for num != 0 {
 		numSlcReverse = append(numSlcReverse, num%10)
 		num /= 10
@@ -23,4 +25,16 @@ func main() {
 	}
 
 	fmt.Print(withoutNum)
+}
+
+func deleteNumSmart(a, b int) {
+	result := 0
+	for i := 1; a > 0; {
+		if a%10 != b {
+			result += a % 10 * i
+			i *= 10
+		}
+		a /= 10
+	}
+	fmt.Println(result)
 }
